02c26feaf1346f19cd8cf7b4da5d2445b59a6410
Exr0n <spotyie@gmail.com>
2020-03-08 19:58:56 -0700

    init commit

---
+++ dontrun_custom/laptop_config.zsh
@@ -0,0 +1,4 @@
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir)
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=1 # https://stackoverflow.com/questions/37286971/shortening-my-prompt-in-zsh
+echo "Laptop config enabled."
+clear
