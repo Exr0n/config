795c6678b664f06d88c6e4f1e46c84d2980cd7e3
Exr0n <spotyie@gmail.com>
2020-08-23 11:10:02 -0700

    better loading note

--- ubersicht/git_status.widget/src/components/StatusList.jsx
+++ ubersicht/git_status.widget/src/components/StatusList.jsx
@@ -13,26 +13,30 @@ class StatusList extends React.Component {
                 this.props.out
                     .trim()
                     .split('\n')
-                    .map(p => { try { return JSON.parse(p) } catch (e) { return 'Loading...'; } } )
+                    .map(p => { try { return JSON.parse(p) } catch (e) { return undefined; } } )
                     .map(p => {
-                        const is_clean = !p[1] && !p[2] && !p[3];
-                        return <div className="status">
-                            <div className="status-icons">
-                                <div className={is_clean ? 'status-icon-clean icon-check' : 'status-icon-dirty icon-chevron-right'} />
-                                <div className={'status-icon-staged icon-pscircle' + (p[3]>0 ? '':' status-hide')}>
-                                    <div className="status-icon-count">{p[3]}</div>
+                        if (p) {
+                            const is_clean = !p[1] && !p[2] && !p[3];
+                            return <div className="status">
+                                <div className="status-icons">
+                                    <div className={is_clean ? 'status-icon-clean icon-check' : 'status-icon-dirty icon-chevron-right'} />
+                                    <div className={'status-icon-staged icon-pscircle' + (p[3]>0 ? '':' status-hide')}>
+                                        <div className="status-icon-count">{p[3]}</div>
+                                    </div>
+                                    <div className={'status-icon-modified icon-plus-sign' + (p[2]>0 ? '':' status-hide')}>
+                                        <div className="status-icon-count">{p[2]}</div>
+                                    </div>
+                                    <div className={'status-icon-untracked icon-question-sign' + (p[1]>0 ? '':' status-hide')}>
+                                        <div className="status-icon-count">{p[1]}</div>
+                                    </div>
                                 </div>
-                                <div className={'status-icon-modified icon-plus-sign' + (p[2]>0 ? '':' status-hide')}>
-                                    <div className="status-icon-count">{p[2]}</div>
-                                </div>
-                                <div className={'status-icon-untracked icon-question-sign' + (p[1]>0 ? '':' status-hide')}>
-                                    <div className="status-icon-count">{p[1]}</div>
+                                <div className="status-path">
+                                    {p[0].split('/').pop()}
                                 </div>
                             </div>
-                            <div className="status-path">
-                                {p[0].split('/').pop()}
-                            </div>
-                        </div>
+                        } else {
+                            return <div className="status-path">Loading...</div>
+                        }
                     })
             }
         </div>
