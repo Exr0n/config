445a92f2b79cd1631019e9d2139ec1caf2fb0ccf
Exr0n <mail@exr0n.com>
2020-11-17 09:20:04 -0800

    auctex; skhd swap spaces

---
+++ .emacs.d/elpa/flymake-aspell-20200902.2226/flymake-aspell-autoloads.el
@@ -0,0 +1,26 @@
+;;; flymake-aspell-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-aspell" "flymake-aspell.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from flymake-aspell.el
+
+(autoload 'flymake-aspell-setup "flymake-aspell" "\
+Enable the spell checker for the current buffer." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-aspell" '("flymake-aspell-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-aspell-autoloads.el ends here
---
+++ .emacs.d/elpa/flymake-aspell-20200902.2226/flymake-aspell-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/albhuan/.emacs.d/elpa/flymake-aspell-20200902.2226/flymake-aspell.el  -*- no-byte-compile: t -*-
+(define-package "flymake-aspell" "20200902.2226" "Aspell checker for flymake" '((emacs "26.1")) :commit "da8c3748228b9c08b518686117163f5da5ea7eb6" :keywords '("wp" "flymake" "spell" "aspell") :authors '(("Leo Gaskin" . "leo.gaskin@brg-feldkirchen.at")) :maintainer '("Leo Gaskin" . "leo.gaskin@brg-feldkirchen.at") :url "https://github.com/leotaku/flycheck-aspell")
