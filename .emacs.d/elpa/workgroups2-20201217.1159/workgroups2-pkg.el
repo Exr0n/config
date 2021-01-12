504b53a056440d2b10d16c1864a926e4f01cf744
Exr0n <mail@exr0n.com>
2021-01-11 16:51:34 -0800

    ignore many dirs

--- .emacs.d/elpa/workgroups2-20201217.1159/workgroups2-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/exr0n/.emacs.d/elpa/workgroups2-20201217.1159/workgroups2.el  -*- no-byte-compile: t -*-
-(define-package "workgroups2" "20201217.1159" "New workspaces for Emacs" '((emacs "25.1")) :commit "c9403c68a7e6491134110d7cacc130c34eae85a0" :authors '(("Sergey Pashinin <sergey at pashinin dot com>")) :maintainer '("Sergey Pashinin <sergey at pashinin dot com>") :keywords '("session" "management" "window-configuration" "persistence") :url "https://github.com/pashinin/workgroups2")
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,11 +27,14 @@
 (require 'smooth-scrolling)
 (smooth-scrolling-mode 1)
 
+;;; large files (vlf)
+(require 'vlf-setup)
+
 ;; background
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
 ;; workgroups
-					; (require 'workgroups2); TODO: FIX
+;; (require 'workgroups2); TODO: FIX
 
 ;; Custom
 (custom-set-variables
@@ -41,8 +44,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("a3b6a3708c6692674196266aad1cb19188a6da7b4f961e1369a68f06577afa16" default))
+ '(org-agenda-files '("~/materials/capsule/org/inbox.org"))
  '(package-selected-packages
-   '(undo-tree aggressive-indent smart-tabs-mode evil-vimish-fold vimish-fold evil-surround workgroups2 smooth-scrolling doom-modeline ivy doom-themes evil)))
+   '(vlf evil-org use-package undo-tree aggressive-indent smart-tabs-mode evil-vimish-fold vimish-fold evil-surround workgroups2 smooth-scrolling doom-modeline ivy doom-themes evil))
+ '(vlf-application 'dont-ask))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -62,11 +67,6 @@
 (require 'evil-vimish-fold)
 (global-evil-vimish-fold-mode 1)
 
-;; (use-package evil-surround              ; https://github.com/emacs-evil/evil-surround
-;;   :ensure t
-;;   :config
-;;   (global-evil-surround-mode 1))
-;; (global-evil-surround-mode 1)
 
 ;; ivy
 (ivy-mode 1)
@@ -101,26 +101,70 @@
 
 ;;; colors
 (load-theme 'doom-challenger-deep)
-;(use-package doom-themes
-;             :config
-;             ;; Global settings (defaults)
-;             (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;                   doom-themes-enable-italic t) ; if nil, italics is universally disabled
-;             (load-theme 'doom-one t)
-;
-;             ;; Enable flashing mode-line on errors
-;             (doom-themes-visual-bell-config)
-;
-;             ;; Enable custom neotree theme (all-the-icons must be installed!)
-;             (doom-themes-neotree-config)
-;             ;; or for treemacs users
-;             (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-;             (doom-themes-treemacs-config)
-;
-;             ;; Corrects (and improves) org-mode's native fontification.
-;             (doom-themes-org-config))
+;; (use-package doom-themes
+;; 			 :config
+;; 			 ;; Global settings (defaults)
+;; 			 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;; 				   doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;; 			 (load-theme 'doom-one t)
 
+;; 			 ;; Enable flashing mode-line on errors
+;; 			 (doom-themes-visual-bell-config)
+
+;; 			 ;; Enable custom neotree theme (all-the-icons must be installed!)
+;; 			 (doom-themes-neotree-config)
+;; 			 ;; or for treemacs users
+;; 			 (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+;; 			 (doom-themes-treemacs-config)
+
+;; 			 ;; Corrects (and improves) org-mode's native fontification.
+;; 			 (doom-themes-org-config))
+
+;; org
+(require 'org)
+(setq org-log-done t)
+(defun agenda-wrapper ()                ; yoniked from quantumish
+  (interactive)
+  (org-agenda nil "a")
+  (org-agenda-month-view)
+  (visual-line-mode -1))
+(global-set-key (kbd "C-c a") 'agenda-wrapper)
+(global-set-key (kbd "C-c t") 'org-todo-list)
+
+(add-hook 'org-agenda-mode-hook	; superuser.com/a/531670
+		  (lambda()
+			(visual-line-mode -1)))
 ;; start at inbox
 (setq initial-buffer-choice (concat (getenv "HOME") "/materials/capsule/org/inbox.org"))
+
+;; evil things
+(use-package evil-org		; https://github.com/Somelauw/evil-org-mode
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+			(lambda ()
+			  (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+(use-package evil-surround              ; https://github.com/emacs-evil/evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+(global-evil-surround-mode 1)
+
+(with-eval-after-load 'evil-maps ; https://emacs.stackexchange.com/questions/46371/how-can-i-get-ret-to-follow-org-mode-links-when-using-evil-mode
+  (define-key evil-motion-state-map (kbd "SPC") nil)
+  (define-key evil-motion-state-map (kbd "RET") nil)
+  (define-key evil-motion-state-map (kbd "TAB") nil))
+(setq org-return-follows-link t)
+
+;; org capture
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; org + latex
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 3.0))
 
 (server-start)
