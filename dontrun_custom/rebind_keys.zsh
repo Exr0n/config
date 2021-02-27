dde139f5d169ef47d23d2f5a418ab8e855778e03
Exr0n <mail@exr0n.com>
2021-02-27 10:26:10 -0800

    type parens, curlies, brackets with modifier taps

--- dontrun_custom/rebind_keys.zsh
+++ dontrun_custom/rebind_keys.zsh
@@ -3,7 +3,8 @@
 # keyboard
 xset r rate 170 40
 setxkbmap -option caps:ctrl_modifier
-xcape -e 'Caps_Lock=Escape'
+killall xcape # reset keybindings
+xcape -e 'Caps_Lock=Escape;Shift_L=parenleft;Shift_R=parenright;Alt_L=bracketleft;Alt_R=bracketright;Super_L=Shift_L|braceleft;Super_R=Shift_L|braceright'
 
 # mouse
 xinput set-prop 'pointer:Razer Razer DeathAdder 2013' 'libinput Natural Scrolling Enabled' 1
